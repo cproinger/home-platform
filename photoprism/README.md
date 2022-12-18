752f661ff009497fd5b6cf61d5c698ae845e82f8
cproinger <christian.proinger@gmail.com>
2022-12-18 11:28:10 +0100

    setup-script & readme for curl-pipe-bash install

---
+++ photoprism/README.md
@@ -0,0 +1,16 @@
+# Prerequisites
+* docker
+* docker-compose
+* cUrl
+
+```
+sudo apt-get install docker
+sudo apt-get install docker-compose
+sudo apt-get install curl
+```
+
+# Setup
+
+```
+curl https://raw.githubusercontent.com/cproinger/home-platform/master/photoprism/setup.sh | sudo /bin/bash
+``` 
--- photoprism/setup.sh
+++ photoprism/setup.sh
@@ -22,4 +22,17 @@ git clone https://github.com/cproinger/home-platform.git
 
 # setup docker compose-stack
 cd /dockerstuff/git/home-platform/photoprism
+echo "setting up your .env-file for the variables PHOTOPRISM_ADMIN_PASSWORD, PHOTOPRISM_MARIADB_PASSWORD and optionally PHOTOPRISM_SITE_URL"
+#echo "then run 'docker-compose -f photoprism-stack.docker-compose.yml up -d'"
+
+echo "enter PHOTOPRISM_ADMIN_PASSWORD"
+read PHOTOPRISM_ADMIN_PASSWORD
+echo "enter PHOTOPRISM_MARIADB_PASSWORD"
+read PHOTOPRISM_ADMIN_PASSWORD
+echo "enter PHOTOPRISM_SITE_URL (or leave blank for ip-address-access only)"
+read PHOTOPRISM_ADMIN_PASSWORD
+echo "writing .env-file"
+printf "PHOTOPRISM_ADMIN_PASSWORD=$PHOTOPRISM_ADMIN_PASSWORD\nPHOTOPRISM_MARIADB_PASSWORD=$PHOTOPRISM_MARIADB_PASSWORD\nPHOTOPRISM_SITE_URL=$PHOTOPRISM_SITE_URL" > .env
+
+echo "starting stack with docker-compose"
 docker-compose -f photoprism-stack.docker-compose.yml up -d
