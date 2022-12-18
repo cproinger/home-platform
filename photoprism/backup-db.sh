41c6d257a3a71a9e029fe2b625274ac7a81491ac
cproinger <christian.proinger@gmail.com>
2022-12-18 19:53:41 +0100

    backup-file variable

--- photoprism/backup-db.sh
+++ photoprism/backup-db.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+BACKUP_TS=$(date '+%Y-%m-%d_%H-%M')
+TARGET_FILE=""/dockerstuff/photoprism/backup/photoprism_mariadb_${BACKUP_TS}.sql.gz""
 echo "starting photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
-docker-compose -f /dockerstuff/git/home-platform/photoprism/photoprism-stack.docker-compose.yml exec -T photoprism photoprism backup -i - | gzip > /dockerstuff/photoprism/backup/photoprism_mariadb_$(date '+%Y-%m-%d').sql.gz
+docker-compose -f /dockerstuff/git/home-platform/photoprism/photoprism-stack.docker-compose.yml exec -T photoprism photoprism backup -i - | gzip > $TARGET_FILE
 echo "finished photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
