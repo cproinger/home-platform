2b1d1d29cd226fe1a5a6b1184118938457420851
cproinger <christian.proinger@gmail.com>
2022-12-18 14:47:47 +0100

    write simple backup-log

--- photoprism/backup-db.sh
+++ photoprism/backup-db.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
+echo "starting photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
 docker-compose -f /dockerstuff/git/home-platform/photoprism/photoprism-stack.docker-compose.yml exec -T photoprism photoprism backup -i - | gzip > /dockerstuff/photoprism/backup/photoprism_mariadb_$(date '+%Y-%m-%d').sql.gz
+echo "finished photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
