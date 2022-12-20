0fd67a6222863a2c94bb2c1dbcbc1c7454e85e2d
cproinger <christian.proinger@gmail.com>
2022-12-20 21:35:13 +0100

    backup-originals.sh

--- photoprism/backup-originals.sh
+++ photoprism/backup-originals.sh
@@ -1,3 +1,3 @@
 echo "sourcing bucket-config.sh"
 . /dockerstuff/git/home-platform/photoprism/bucket-config.sh
-aws s3 sync /dockerstuff/photoprism/originals s3://$BUCKET_NAME/originals --storage-class DEEP_ARCHIVE
+aws s3 sync /dockerstuff/photoprism/originals s3://$BUCKET_NAME/originals --storage-class DEEP_ARCHIVE --no-progress
