934802d34313931493f1dd47ab75a30f5b986efa
cproinger <christian.proinger@gmail.com>
2022-12-22 19:55:49 +0100

    backup-db gzip best-compression

--- photoprism/backup-db.sh
+++ photoprism/backup-db.sh
@@ -2,7 +2,7 @@
 BACKUP_TS=$(date '+%Y-%m-%d_%H-%M')
 TARGET_FILE="/dockerstuff/photoprism/backup/photoprism_mariadb_${BACKUP_TS}.sql.gz"
 echo "starting photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
-docker-compose -f /dockerstuff/git/home-platform/photoprism/photoprism-stack.docker-compose.yml exec -T photoprism photoprism backup -i - | gzip > $TARGET_FILE
+docker-compose -f /dockerstuff/git/home-platform/photoprism/photoprism-stack.docker-compose.yml exec -T photoprism photoprism backup -i - | gzip -9 > $TARGET_FILE
 echo "finished photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
 
 echo "sourcing bucket-config.sh"
