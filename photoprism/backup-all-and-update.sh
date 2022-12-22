98cfdd0660d722be0bebf98b860f704430f69bdf
cproinger <christian.proinger@gmail.com>
2022-12-22 20:52:48 +0100

    backup-all-and-update.sh log timestamps

--- photoprism/backup-all-and-update.sh
+++ photoprism/backup-all-and-update.sh
@@ -1,2 +1,4 @@
+echo "starting backup-all-and-update at $(date '+%Y-%m-%d_%H-%M')"
 . /dockerstuff/git/home-platform/photoprism/backup-all.sh
 . /dockerstuff/git/home-platform/photoprism/update.sh
+echo "finished backup-all-and-update at $(date '+%Y-%m-%d_%H-%M')"
