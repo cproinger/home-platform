dea546bdec48bd77356b643b03f62a89aae31e37
cproinger <christian.proinger@gmail.com>
2024-04-21 19:03:51 +0200

    backup-db.sh appends to logfile

--- photoprism/backup-db.sh
+++ photoprism/backup-db.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 BACKUP_TS=$(date '+%Y-%m-%d_%H-%M')
 TARGET_FILE="/dockerstuff/photoprism/backup/photoprism_mariadb_${BACKUP_TS}.sql.gz"
-echo "starting photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
+echo "starting photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" >> /dockerstuff/photoprism/backup/backup.log
 docker-compose -f /dockerstuff/git/home-platform/photoprism/photoprism-stack.docker-compose.yml exec -T photoprism photoprism backup -i - | gzip -9 > $TARGET_FILE
-echo "finished photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" > /dockerstuff/photoprism/backup/backup.log
+echo "finished photoprism-mariadb-backup at $(date '+%Y-%m-%d_%H-%M')" >> /dockerstuff/photoprism/backup/backup.log
 
 echo "sourcing bucket-config.sh"
 . /dockerstuff/git/home-platform/photoprism/bucket-config.sh
